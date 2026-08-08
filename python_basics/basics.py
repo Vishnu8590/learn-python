5818d165092eafbaf1a0b5fda6d27dd9d2e448db
Vishnu PP <vishnupp8590@gmail.com>
2026-08-08 20:49:44 +0530

    Add Python basics practice

---
+++ python_basics/basics.py
@@ -0,0 +1,40 @@
+# 1. Display Message
+print("Welcome to Python")
+
+# Output:
+# Welcome to Python
+
+
+# 2. Basic Calculations
+a = 10
+b = 20
+print("add:",a + b)
+print("sub:",a-b)
+print("mult:",a * b)
+print("div:",a / b)
+
+
+# Output:
+# add: 30
+# sub: -10
+# mult: 200
+# div: 0.5
+
+
+# 1. Data Types
+name = "Vishnu"
+id=12.0
+age = 20
+is_learning_python = True
+
+
+print(type(name))
+print(type(id))
+print(type(age))
+print(type(is_learning_python))
+
+# Output:
+# <class 'str'>
+# <class 'float'>
+# <class 'int'>
+# <class 'bool'>
