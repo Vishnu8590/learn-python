0de892e75779e721aa4e2ed5c250b75c63ea7f76
Vishnu PP <vishnupp8590@gmail.com>
2026-08-09 11:42:13 +0530

    Add additional examples for variable assignment, string operations, and user input

--- python_basics/basics.py
+++ python_basics/basics.py
@@ -39,4 +39,102 @@ print(type(is_learning_python))
 # <class 'int'>
 # <class 'bool'>
 
+#Defining a variable
 
+a =10
+print(a)
+
+# Output:
+#10
+
+#variable Assignment
+a=1
+print(a)
+a=2
+print(a)
+
+# Output:
+# 1
+# 2
+
+#value in variable
+
+a=1
+b=2
+a=b
+print(a)
+print(b)
+
+# Output:
+#2
+#2
+
+#Order of Operations(BODMAS)
+
+print(10/2+3)
+
+# Output:
+#8.0
+
+#Input and Output Basics
+
+#Adding strings
+
+a = "1"+"2"
+print(a)
+
+# Output:
+#12
+
+#String Concatenation
+
+name = "Vishnu"
+print("Hi "+name)
+
+# Output:
+#Hi Vishnu
+
+a="*" + "*"
+print(a)
+
+# Output:
+#**
+
+a= "*" *5
+print(a)
+
+# Output:
+#*****
+
+#String Repetition
+
+s ="Python"
+print("*"*3 + s + "*"*3)
+
+#Output
+#***Python***
+
+
+#Length of string
+a="Ravi"
+print(len(a))
+
+# Output:
+#4
+
+#Take input from user
+
+username= input()
+age = input()
+print(username +" is "+age+" years old.")
+
+# Output:
+#Vishnu is 20 years old
+
+#Accessing Characters in string
+
+a = "Ravi"
+print(a[0])
+
+#Output
+#R
